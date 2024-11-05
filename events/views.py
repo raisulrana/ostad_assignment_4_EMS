17ba2979ddbdf5c6c2154054ba9a3703aaeace41
raisulrana <raisul.rana16@gmail.com>
2024-11-05 21:13:12 +0600

    Event Booking Updated

--- events/views.py
+++ events/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Event, Category
-from django.http import HttpResponseForbidden
 from .forms import EventForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseForbidden
+from .models import Event, Booking
 
 # Create your views here.
 @login_required
@@ -26,36 +27,17 @@ def add_event(request):
 @login_required
 def edit_event(request, id):
     edit_event = Event.objects.get(pk=id)
-    event_form = EventForm(instance=edit_event)
+
     if request.method == 'POST':
-        event_form = EventForm(request.POST)
+        event_form = EventForm(request.POST, instance=edit_event)
         if event_form.is_valid():
             event_form.save()
             return redirect('homepage')
-        else:
-            event_form = EventForm()
     else:
-        event_form = EventForm()
+        event_form = EventForm(instance=edit_event)
         
     return render(request, 'events/edit_event.html',{'form': event_form})
 
-# @login_required
-# def edit_event(request, id):
-#     edit_event = get_object_or_404(Event, pk=id)
-    
-#     # Check if the user is the creator or an admin
-#     if request.user != edit_event.created_by and not request.user.is_staff:
-#         return HttpResponseForbidden("You do not have permission to edit this event.")
-
-#     if request.method == 'POST':
-#         event_form = EventForm(request.POST, instance=edit_event)
-#         if event_form.is_valid():
-#             event_form.save()
-#             return redirect('homepage')
-#     else:
-#         event_form = EventForm(instance=edit_event)
-    
-#     return render(request, 'events/add_event.html', {'form': event_form})
 
 @login_required
 def delete_event(request, id):
@@ -63,3 +45,26 @@ def delete_event(request, id):
     delete_event.delete()
     
     return redirect ('homepage')
+
+
+@login_required
+def book_event(request, id):
+    event = get_object_or_404(Event, pk=id)
+    
+    # Check if the user is the event creator
+    if request.user == event.created_by:
+        return HttpResponseForbidden("You cannot book your own event.")
+    
+    # Check if the user has already booked this event
+    if Booking.objects.filter(event=event, user=request.user).exists():
+        return HttpResponse("You have already booked this event.")
+    
+    # Create a booking record
+    Booking.objects.create(event=event, user=request.user)
+    return redirect('homepage')  # Redirect to a confirmation page if needed
+
+
+@login_required
+def user_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'events/user_bookings.html', {'bookings': bookings})
